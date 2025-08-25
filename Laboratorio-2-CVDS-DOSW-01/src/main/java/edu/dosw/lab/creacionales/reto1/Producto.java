beb430ef17ca8ba1a83561ee383997df7a38e57d
andrew <andrewc7480@gmail.com>
2025-08-25 12:08:06 -0500

    Retos adelanto

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/creacionales/reto1/Producto.java
@@ -0,0 +1,19 @@
+package edu.dosw.lab.creacionales.reto1;
+
+public class Producto {
+    private String nombre;
+    private int precio;
+
+    public Producto(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+}
