d5f82265413b9b1a9f9f426730427f2c38cdca94
Camilo <juan.cristancho-v@mail.escuelaing.edu.co>
2025-08-25 23:42:37 -0500

    reto 5 cafes personalizados

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/solid/reto5/Cafe.java
@@ -0,0 +1,72 @@
+package edu.dosw.lab.solid.reto5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Cafe {
+    private double precio;
+    private TreeMap<Integer, String> nombresToppings;
+    private TreeMap<Integer, Double> preciosToppings;
+    private ArrayList toppingsUsados;
+
+    public Cafe(String[] toppings) {
+        precio = 2000.0; // Precio base del cafe
+        toppingsUsados = new ArrayList();
+        
+        // TreeMap para nombres de toppings
+        nombresToppings = new TreeMap<>();
+        nombresToppings.put(1, "Leche");
+        nombresToppings.put(2, "Chocolate");
+        nombresToppings.put(3, "Caramelo");
+        nombresToppings.put(4, "Crema Batida");
+        nombresToppings.put(5, "Menta");
+
+        // TreeMap para precios de toppings
+        preciosToppings = new TreeMap<>();
+        preciosToppings.put(1, 1000.0);
+        preciosToppings.put(2, 1500.0);
+        preciosToppings.put(3, 1200.0);
+        preciosToppings.put(4, 2000.0);
+        preciosToppings.put(5, 1300.0);
+
+        agregarToppings(toppings);
+        
+    }
+    private void agregarToppings(String[] toppings) {
+        
+        for (String topping : toppings) {
+            int toppingNumber = Integer.parseInt(topping.trim());
+            if (toppingNumber == 6){
+                Scanner scs = new Scanner(System.in);
+                System.out.print("Ingrese el nombre del nuevo topping: ");
+                String nombre = scs.nextLine();
+                toppingsUsados.add(nombre);
+
+                System.out.print("Ingrese el precio del nuevo topping: ");
+                double precioTopping = scs.nextDouble();
+                this.precio += precioTopping;
+                
+            }else{
+                this.precio += preciosToppings.get(toppingNumber);
+                toppingsUsados.add(nombresToppings.get(toppingNumber));
+            }
+            
+
+        }
+    }
+
+
+    public double getPrecio() {
+        return precio;
+    }
+    public TreeMap<Integer, String> getNombresToppings() {
+        return nombresToppings;
+    }
+    public TreeMap<Integer, Double> getPreciosToppings() {
+        return preciosToppings;
+    }
+    public ArrayList getToppingsUsados() {
+        return toppingsUsados;
+    }
+}
---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/solid/reto5/Tienda.java
@@ -0,0 +1,34 @@
+package edu.dosw.lab.solid.reto5;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tienda {          
+    private static ArrayList<Cafe> cafes = new ArrayList<>();
+    private double preciototal;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Numeros de cafes a personalizar: ");
+        int numero = sc.nextInt();
+        for(int i = 0; i < numero; i++){
+            Scanner scs = new Scanner(System.in);
+            System.out.print("Seleccione toppings (numeros separados por coma): \n");
+            String input = scs.nextLine();
+            String[] toppingNumbers = input.split(",");
+            Cafe cafe = new Cafe(toppingNumbers);
+            cafes.add(cafe);
+        }
+
+        double preciototal = 0;
+        for (int j = 0; j < cafes.size(); j++) {
+            System.out.println("--- Cafe " + (j+1) + "---");
+            System.out.println("Toppings: " + cafes.get(j).getToppingsUsados());
+            System.out.println("Precio: " + cafes.get(j).getPrecio() + "\n");
+            preciototal += cafes.get(j).getPrecio();
+        }
+        System.out.println("--------------------------------");
+        System.out.println("Total a pagar por todos los cafés: " + preciototal);
+        System.out.println("Disfrute su café!");
+    }
+
+    
+}
